43eeaf69b27510d7a31cba7194ed2d308e167600
theo ballet <theo@integnano-theo.c2n.u-psud.fr>
2023-04-04 10:03:58 +0200

    added dbg

--- sim/verilator/Pinaipple_system_main.cc
+++ sim/verilator/Pinaipple_system_main.cc
@@ -3,17 +3,19 @@
 
 #include "Vpinaipple_system.h"
 #include "verilated.h"
+#include <iostream>
 
 int main(int argc, char **argv){
     VerilatedContext *contextp = new VerilatedContext;
-
+    
     contextp->commandArgs(argc, argv);
 
     Vpinaipple_system *top = new Vpinaipple_system{contextp};
-
+    std::cout << "Starting simulation" << std::endl;
     while (!contextp->gotFinish()) {
         top->eval();
     }
+    std::cout << "Simulation ended" << std::endl;
 
     delete top;
     delete contextp;
