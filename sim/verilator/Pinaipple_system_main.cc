36c7c89fa637c9a77a07a2310a44d0235b805bba
theo ballet <theo@integnano-theo.c2n.u-psud.fr>
2023-04-04 10:10:50 +0200

    TODO change verilator main

--- sim/verilator/Pinaipple_system_main.cc
+++ sim/verilator/Pinaipple_system_main.cc
@@ -6,18 +6,10 @@
 #include <iostream>
 
 int main(int argc, char **argv){
-    VerilatedContext *contextp = new VerilatedContext;
-    
-    contextp->commandArgs(argc, argv);
+    PinaippleSystem testsystem(
+    // TODO: add the path to the RAM 
 
-    Vpinaipple_system *top = new Vpinaipple_system{contextp};
-    std::cout << "Starting simulation" << std::endl;
-    while (!contextp->gotFinish()) {
-        top->eval();
-    }
-    std::cout << "Simulation ended" << std::endl;
+    )
 
-    delete top;
-    delete contextp;
-    return 0;
+    return testsystem.Main(argc, argv);
 }
